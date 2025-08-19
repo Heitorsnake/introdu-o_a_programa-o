9a1f922a9bed92c837441da04c34f11241ca812e
Heitor Pernet <heitorpernet@gmail.com>
2025-08-19 08:13:04 -0300

    Add files via upload

---
+++ testes2.py
@@ -0,0 +1,8 @@
+import random
+n1=float(random.randint(1, 10))
+n2=float(random.randint(1, 10))
+print(f"nota do 1° bimestre:{n1}")
+print(f"nota do 2° bimestre:{n2}")
+media=float(((60-(n1*2+n1*2))/2)/3)
+print(f"ele precisa tirar no 3° bimestre, {media} no mínimo")
+print(f"ele precisa tirar no 4° bimestre, {media} no mínimo")
