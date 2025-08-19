b6447316bec888f71c3671c64ef8e439280c37f9
Heitor Pernet <heitorpernet@gmail.com>
2025-08-19 08:46:20 -0300

    Update testes2.py

--- testes2.py
+++ testes2.py
@@ -1,8 +1,5 @@
-import random
-n1=float(random.randint(1, 10))
-n2=float(random.randint(1, 10))
-print(f"nota do 1° bimestre:{n1}")
-print(f"nota do 2° bimestre:{n2}")
-media=float(((60-(n1*2+n1*2))/2)/3)
-print(f"ele precisa tirar no 3° bimestre, {media} no mínimo")
-print(f"ele precisa tirar no 4° bimestre, {media} no mínimo")
+pares = 0
+for i in range(0, 10):
+    if i % 2 == 0:
+        pares+=1
+print(f"Quantidade de números pares entre 0 e 10: {pares}")
