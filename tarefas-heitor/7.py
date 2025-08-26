a26954cd7cb1c995e34c63f08f09fd8d05184a0f
Heitor Pernet <heitorpernet@gmail.com>
2025-08-26 08:40:45 -0300

    Update 7.py

--- tarefas-heitor/7.py
+++ tarefas-heitor/7.py
@@ -1,12 +1,18 @@
 thanos = 150
 aranha = 110
-for i in range(100):
-    thanos = thanos + 2
-    aranha = aranha + 3
-    if aranha > thanos:
-        print(f"{i} é a quantidade de anos que demorou para o aranha ultrapassar o thanos")
-        print(f"{aranha} é a altura do aranha")
-        print(f"{thanos} é a altura do thanos")
-        break
-        
+anos = 0
+
+print(f"Antes: aranha = {aranha}, thanos = {thanos}")
+
+if aranha > thanos:
+    anos = 0
+else:
+    anos = (thanos - aranha) // (3 - 2) +1
+    aranha += anos * 3
+    thanos += anos * 2
+
+print(f"Depois: aranha = {aranha}, thanos = {thanos}")
+print(f"{anos} é a quantidade de anos que demorou para o aranha ultrapassar o thanos")
+print(f"{aranha} é a altura do aranha")
+print(f"{thanos} é a altura do thanos")
         
