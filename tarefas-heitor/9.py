80395409f1449332e8997ab6904600f948c7f052
Heitor Pernet <heitorpernet@gmail.com>
2025-08-26 08:39:49 -0300

    Update 9.py

--- tarefas-heitor/9.py
+++ tarefas-heitor/9.py
@@ -1,13 +1,9 @@
-n = int(float(input("Digite um número: ")))
-eh_primo = True
-if n < 2:
-    eh_primo = False
-else:
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            eh_primo = False
-            break
-if eh_primo:
+p=int(input("Digite um número: "))
+contador=0
+for i in range(1,p+1):
+    if p % i == 0:
+        contador+=1
+if contador == 2:
     print("O número é primo")
 else:
     print("O número não é primo")
