971d6538f07e00e9f4b1758aa357b164b555d442
Heitor Pernet <heitorpernet@gmail.com>
2025-08-26 08:11:08 -0300

    Delete testes.py

--- testes.py
+++
@@ -1,6 +0,0 @@
-n1=float(input("fale sua nota do 1° bimestre:"))
-n2=float(input("fale sua nota do 2° bimestre:"))
-media3=float(((60-(n1*2+n1*2))/2)/3)
-print(f"ele precisa tirar no 3° bimestre:{media3}")
-print(f"ele precisa tirar no 4° bimestre:{media3}")
-
