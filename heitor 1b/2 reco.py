cbe91aa2a85969f1905b6c922d324c06b66b16c9
Heitor Pernet <heitorpernet@gmail.com>
2025-09-11 10:15:08 -0300

    Delete heitor 1b directory

--- heitor 1b/2 reco.py
+++
@@ -1,8 +0,0 @@
-pares=0
-valores=100
-while valores <201:
-    if valores % 2 != 0:
-        valores+=1
-    else:
-        valores += 1
-        print(valores)
--- heitor 1b/3 reco.py
+++
@@ -1,7 +0,0 @@
-N = int(float(input("Digite um número: ")))
-soma = 0
-m=1
-while N >0:
-    if N>m:
-        m+=m
-print("A soma dos números inteiros de 1 a", N, "é:", m)
