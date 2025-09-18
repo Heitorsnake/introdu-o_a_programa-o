e21da555acc5f1fe53a301de8d5117c12e5bea73
Heitor Pernet <heitorpernet@gmail.com>
2025-09-18 10:27:01 -0300

    Update recomeço de arquivo.py

--- recomeço de arquivo.py
+++ recomeço de arquivo.py
@@ -1,4 +1,3 @@
-
 while 1==1:
     print("O P E R A Ç Õ E S   M A T E M Á T I C A S")
     print("***************M E N U*****************")
@@ -6,52 +5,71 @@ while 1==1:
     print("2 - subtração")
     print("3 - divisão")
     print("4 - multiplicação")
-    print("5 - par ou ínpar")
-    print("6 -factorial")
+    print("5 - par ou ímpar")
+    print("6 - fatorial")
     print("7 - primo ou não")
     print("8 - sair")
-    menu=input("digite oque deseja fazer:")
+    menu=int(input("digite aqui, qual das operações matemáticas você deseja fazer:"))
     if menu==1:
-        num11=input("digite um número:")
-        num12=input("digite um número:")
+        num11=float(input("digite um número:"))
+        num12=float(input("digite um número:"))
         num13=num11+num12
-        print("a soma dos números é:",num13)
+        print("a soma dos  dois números é:",num13)
+        input("aperte qualquer tecla pra voltar para o menu")
     if menu==2:
-        num21=input("digite um número:")
-        num22=input("digite um número:")
-        num23=num11-num12
-        print("a subtração dos números é:",num23)
+        num21=float(input("digite um número:"))
+        num22=float(input("digite um número:"))
+        num23=num21-num22
+        print("a subtração dos dois números é:",num23)
+        input("aperte qualquer tecla pra voltar para o menu")
     if menu==3:
-        num31=input("digite um número:")
-        num32=input("digite um número:")
+        num31=float(input("digite um número:"))
+        num32=float(input("digite um número:"))
         num33=num31/num32
         print("a divisão dos dois número é:",num33)
+        input("aperte qualquer tecla pra voltar para o menu")
+        
     if menu==4:
-        num41=input("digite um número:")
-        num42=input("digite um número:")
+        num41=float(input("digite um número:"))
+        num42=float(input("digite um número:"))
         num43=num41*num42
         print("a mutiplicação dos dois número é:",num43)
+        input("aperte qualquer tecla pra voltar para o menu")   
     if menu==5:
         num51=int(input("digite um número:"))
         if num51%2==0:
             print("o número é par")
+            input("aperte qualquer tecla pra voltar para o menu")
         else:
-            print("o número não é par")
+            print("o número é ímpar")
+            input("aperte qualquer tecla pra voltar para o menu")
     if menu==6:
-      N = int(float(input("Digite um número: ")))
-      fator = 1
-      for i in range(1, N+0):
-        fator *= i
-        print(f"O fatorial de {N} é {fator}")  
+        N = int(input("Digite um número: "))
+        fator = 1
+        i=1
+        while i<N+1:
+            fator *= i
+            i+=1
+            print(f"O fatorial de {N} é {fator}")
+            input("aperte qualquer tecla pra voltar para o menu")
     if menu==7:
         p=int(input("Digite um número: "))
-        contador=0
-    for i in range(1,p+1):
-        if p % i == 0:
-            contador+=1
-        if contador == 2:
-            print("O número é primo")
-        else:
-            print("O número não é primo")
-    if menu==8:
-        breakpoint
+        c=0
+        i=1
+        while i<=p:
+            if p % i == 0:
+                c+=1
+                i+=1
+            if  c ==2:
+                print("o número é primo!")
+                input("aperte qualquer tecla pra voltar para o menu")
+            else:
+                print("o número não é primo")
+                input("aperte qualquer tecla pra voltar para o menu")
+    if menu ==8:
+        print("até a proxima :)")
+        break
+    if menu !=(1,8):
+        input("aperte enter para tentar novamente")
+    if menu !=int:
+        input("aperte enter para tentar novamente")
